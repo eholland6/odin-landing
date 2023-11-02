c5c05d2b47d466f6ac42fdc571bdfa089a49a55d
Eric Holland <hollanderic@yahoo.com>
2023-11-01 21:52:44 -0400

    Created first cut of game to played in console # Please enter the commit message for your changes. Lines starting

---
+++ js_exercises/rock_paper_scissors/javascript.js
@@ -0,0 +1,59 @@
+function getComputerChoice() {
+    let rand = Math.random();
+    if (rand < 0.33) {
+        computerChoice = 'Rock';
+    }
+    else if (rand < 0.66) {
+        computerChoice = 'Paper';
+    }
+    else {
+        computerChoice = 'Scissors';
+    }
+
+    return computerChoice;
+}
+
+function playRound(playerChoice) {
+    playerChoice = firstLetterCaps(playerChoice);
+    computerChoice = getComputerChoice();
+
+    if (playerChoice == computerChoice) {
+        result = `Tie! You both chose ${playerChoice}. So unsatisfying!`
+    }
+    else if (playerChoice == 'Rock') {
+        if (computerChoice == 'Paper') {
+            result = 'You lose! Paper covers Rock!'
+        }
+        else {
+            result = `You win! Rock smashes Scissors!`
+        }
+    }
+    else if (playerChoice == 'Paper') {
+        if (computerChoice == 'Scissors') {
+            result = 'You Lose! Scissors cuts that paper all up!'
+        }
+        else {
+            result = 'You Win! Paper covers that Rock right up!'
+        }
+    }
+    else if (playerChoice == 'Scissors') {
+        if (computerChoice == 'Rock') {
+            result = 'You Lose! Rock smases your scissors big time!'
+        }
+        else {
+            result = "You win! Your scissors cut up the computer's paper"
+        }
+    }
+
+    return result
+}
+
+function firstLetterCaps(textStr) {
+    lenStr = textStr.length;
+    firstLetter = textStr[0].toUpperCase();
+    restOfWord = textStr.slice(1, (lenStr));
+    restOfWord = restOfWord.toLowerCase();
+    newStr = `${firstLetter}${restOfWord}`
+    
+    return newStr
+}
